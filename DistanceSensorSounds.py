3249ad65e62f51645f97bdd6d2edd615a1c1b93d
N0TA06IN <87941103+N0TA06IN@users.noreply.github.com>
2021-08-07 16:01:37 -0400

    Update DistanceSensorSounds.py

--- DistanceSensorSounds.py
+++ DistanceSensorSounds.py
@@ -1,4 +1,5 @@
-from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
+#Import
+mfrom spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
 from spike.control import wait_for_seconds, wait_until, Timer
 from math import *
 from spike import DistanceSensor
@@ -6,6 +7,7 @@ from spike import DistanceSensor
 hub = PrimeHub()
 distance_sensor = DistanceSensor('C')
 
+#Execute
 while True:
     distance_sensor.wait_for_distance_farther_than(10, 'in')
     hub.speaker.start_beep(70)
